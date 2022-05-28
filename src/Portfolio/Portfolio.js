efb7f8e7a70fee28feabdbffce751a9249ae1160
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-05-28 21:44:22 +0600

    deploy ready

--- src/Portfolio/Portfolio.js
+++ src/Portfolio/Portfolio.js
@@ -1,26 +1,24 @@
-import React from 'react'
+import React from "react";
 
-const Portfolio=()=> {
-    return (
-        <div>
-            <div class="card card-side mt-20 bg-base-100 shadow-xl mx-12">
-
+const Portfolio = () => {
+  return (
+    <div>
+      <div class="card card-side mt-20 bg-base-100 shadow-xl mx-12">
         <div class="card-body">
-          <h2 class="card-title">Name : Afsana Ara Bristy</h2>
+          <h2 class="card-title">Name: Afsana Ara Bristy</h2>
           <h2> Email Address : afsanaara928@gmail.com</h2>
           <h2> Education : Computer Science and Engineering</h2>
           <h2> Skills : HTML, CSS, JavaScript,React,Firebase,NodeJS,Mongodb</h2>
           <h2>
             {" "}
             Projects : Go panda :https://go-panda-37deb.web.app/, <br />
-            Ara Planner :https://ara-planner.web.app/{" "}
-            <br />
+            Ara Planner :https://ara-planner.web.app/ <br />
             Gucci : https://rococo-brigadeiros-5202e3.netlify.app/
           </h2>
         </div>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
