d371de4666e69b9a7cb63ca42a1921f2f7f0a01c
Afsana Ara Bristy <afsanaara928@gmail.com>
2022-05-28 15:58:20 +0600

    portfolio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Blogs from "./Blogs/Blogs";
+import Portfolio from "./Portfolio/Portfolio";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Portfolio/Portfolio.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Portfolio=()=> {
+    return (
+        <div>
+            <div class="card card-side mt-20 bg-base-100 shadow-xl mx-12">
+
+        <div class="card-body">
+          <h2 class="card-title">Name : Afsana Ara Bristy</h2>
+          <h2> Email Address : afsanaara928@gmail.com</h2>
+          <h2> Education : Computer Science and Engineering</h2>
+          <h2> Skills : HTML, CSS, JavaScript,React,Firebase,NodeJS,Mongodb</h2>
+          <h2>
+            {" "}
+            Projects : Go Travel : https://go-travel-c2715.web.app, <br />
+            Puma Bangladesh : https://amazing-taffy-10a00e.netlify.app/home{" "}
+            <br />
+            Powersurge Supplements : https://powersurge-supplements.web.app/
+          </h2>
+        </div>
+      </div>
+        </div>
+    )
+}
+
+export default Portfolio
